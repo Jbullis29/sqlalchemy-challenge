834f1b5a64b09f736944d2dd77a9c0e57858d6e3
Jbullis29 <jsbullis16@gmail.com>
2021-05-13 19:47:55 -0600

    Added start and end links to my api

--- app.py
+++ app.py
@@ -28,7 +28,9 @@ def home():
         f"The following routes are available: <br/>"
         f'/api/v1.0/precipitation <br/>'
         f'/api/v1.0/stations <br/>'
-        f'/api/v1.0/tobs'
+        f'/api/v1.0/tobs <br/>'
+        f'/api/v1.0/<start> <br/>'
+        f'/api/v1.0/<start>/<end>'
         )
 
 @app.route("/api/v1.0/precipitation")
@@ -58,6 +60,30 @@ def tobs():
     filter(precipitation.station == 'USC00519281', precipitation.date >= start_date).all()
     temps_list = [{'station':temp[0], 'date':temp[1], 'temp':temp[2]} for temp in temps]
     return jsonify(temps_list)
+
+@app.route('/api/v1.0/<start>')
+def analytics (start):
+    
+    user_start = sesh.query(func.min(precipitation.date), func.min(precipitation.tobs), 
+    func.avg(precipitation.tobs), func.max(precipitation.tobs)).\
+        filter(precipitation.date >= start)
+    results = {'Start Date': user_start[0][0],  'Minimum recording': user_start[0][1],
+    'Maximum Recording': user_start[0][3], 'Average Recording': user_start[0][2]}
+    sesh.close()
+    return jsonify(results)
+
+@app.route('/api/v1.0/<start>/<end>')
+def analytics2 (start,end):
+    
+    user_start = sesh.query(func.min(precipitation.date),func.max(precipitation.date),
+    func.min(precipitation.tobs), func.avg(precipitation.tobs), func.max(precipitation.tobs)).\
+        filter(precipitation.date >= start).\
+            filter(precipitation.date <= end)
+    results = {'Minimum recording': user_start[0][2], 'Maximum Recording': user_start[0][4], 
+    'Average Recording': user_start[0][3],'Start Date': user_start[0][0], 
+    'End Date': user_start[0][1]}
+    sesh.close()
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
